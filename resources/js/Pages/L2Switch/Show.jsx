32acd4c78c968b8fb7f5afb3db64a7b205eb63d6
lmc-git-repo <162035523+zellyar@users.noreply.github.com>
2025-09-02 17:17:17 +0800

    Added L2Switch Features

---
+++ resources/js/Pages/L2Switch/Show.jsx
@@ -0,0 +1,44 @@
+// Components-ModalComponent.jsx
+import { Modal, Button } from 'flowbite-react';
+import { forwardRef } from 'react';
+
+const ModalComponent = forwardRef(function ModalComponent({ show, onClose, l2switch }, ref) {
+  if (!l2switch) return null;
+
+  return (
+    <Modal show={show} onClose={onClose}>
+      <Modal.Header className="p-4">
+        {l2switch.device_name}
+      </Modal.Header>
+      <Modal.Body>
+        <div className="space-y-6">
+            <div className='text-center'>
+                <p className="text-base leading-relaxed text-white"><strong>{l2switch.device_name.toUpperCase()}</strong></p>
+            </div>
+            <div className="flex justify-around p-1">
+                <div className="rounded p-3 w-full">
+                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400"><strong>ID:</strong> {l2switch.id}</p>
+                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400"><strong>Model:</strong> {l2switch.model}</p>
+                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400"><strong>IP Address:</strong> {l2switch.ip_address}</p>
+                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400"><strong>Username:</strong> {l2switch.username}</p>
+                </div>
+                <div className="rounded p-3 w-full">
+                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400"><strong>Password:</strong> {l2switch.password}</p>
+                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400"><strong>Created By:</strong> {l2switch.created_by?.name || 'N/A'}</p>
+                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400"><strong>Created At:</strong> {l2switch.created_at ? new Date(l2switch.created_at).toISOString().split('T')[0] : 'N/A'}</p>
+                </div>
+            </div>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={onClose} color="blue">
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+});
+
+ModalComponent.displayName = 'L2SwitchShowModal';
+
+export default ModalComponent;
