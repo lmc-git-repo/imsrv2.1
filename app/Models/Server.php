43af9ade2a1604074e4afa2a64560bf2fbdf7b19
lmc-git-repo <larracasliezel31@gmail.com>
2025-09-05 13:56:38 +0800

    add server model, controller, and requests

---
+++ app/Models/Server.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Server extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'device_name',
+        'model',
+        'ip_address',
+        'username',
+        'password',
+        'asset_tag',
+        'created_by',
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
