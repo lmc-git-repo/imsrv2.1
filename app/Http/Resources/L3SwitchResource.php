e75d855dfb7321ed7f8920ba810737c71dd8fb3e
liezel <larracasliezel31@gmail.com>
2025-09-09 11:06:47 +0800

    additional resource for Consistent Data Serialization

---
+++ app/Http/Resources/L3SwitchResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class L3SwitchResource extends JsonResource
+{
+    public static $wrap = false;
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'device_name' => $this->device_name,
+            'model' => $this->model,
+            'ip_address' => $this->ip_address,
+            'username' => $this->username,
+            'password' => $this->password,
+            'createdBy' => $this->whenLoaded('createdBy', function () {
+                return [
+                    'id' => $this->createdBy->id,
+                    'name' => $this->createdBy->name,
+                    'email' => $this->createdBy->email,
+                    'role' => $this->createdBy->role,
+                ];
+            }),
+            'created_at' => (new Carbon($this->created_at))->format('Y-m-d'),
+        ];
+    }
+}
