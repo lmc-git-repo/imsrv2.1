12eaaaae90295ef153eae2d8f2e0fcb6e8768616
liezel <larracasliezel31@gmail.com>
2025-09-10 13:11:46 +0800

    update migration tables

--- database/migrations/2025_09_02_085050_create_firewalls_table.php
+++ database/migrations/2025_09_02_085050_create_firewalls_table.php
@@ -12,17 +12,18 @@ return new class extends Migration
     public function up(): void
 {
     Schema::create('firewalls', function (Blueprint $table) {
-        $table->id();
-        $table->string('device_name');
-        $table->string('model')->nullable();
-        $table->string('ip_address');
-        $table->string('username')->nullable();
-        $table->string('password')->nullable();
-        $table->unsignedBigInteger('created_by');
-        $table->timestamps();
-        // Foreign key constraint
-        $table->foreign('created_by')->references('id')->on('users');
-    });
+    $table->id();
+    $table->string('device_name');
+    $table->string('model')->nullable();
+    $table->string('ip_address');
+    $table->string('username')->nullable();
+    $table->string('password')->nullable();
+    $table->string('serial_number')->nullable();
+    $table->unsignedBigInteger('created_by');
+    $table->timestamps();
+    // Foreign key constraint
+    $table->foreign('created_by')->references('id')->on('users');
+});
 }
 
     /**
